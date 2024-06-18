49ecf9adf35b8c1dabff2e66d24b9c716e836c8b
LesnoyPudge <chuykin.a01@gmail.com>
2024-06-18 17:38:46 +0500

    'update'

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,2 +1,3 @@
 export { default as defaults } from 'defaults';
 export { default as autoBind } from 'auto-bind';
+export { pipe } from 'ts-functional-pipe';
--- src/other/omit/omit.ts
+++ src/other/omit/omit.ts
@@ -3,12 +3,12 @@ import { T } from '@lesnoypudge/types-utils-base';
 
 
 export const omit = <
-    _Source extends T.AnyRecord,
+    _Source extends T.UnknownRecord,
     _Keys extends keyof _Source,
 >(
     source: _Source,
     ...keys: T.NonEmptyArray<_Keys>
-): T.Prettify<Omit<_Source, _Keys>> => {
+): T.Simplify<Omit<_Source, _Keys>> => {
     return Object.keys(source).reduce<Omit<_Source, _Keys>>((acc, cur) => {
         if (keys.includes(cur)) return acc;
         // @ts-ignore
