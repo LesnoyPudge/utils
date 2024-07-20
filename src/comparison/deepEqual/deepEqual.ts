a09142452db5dbca651c5a06c41a206d9cef8cec
LesnoyPudge <chuykin.a01@gmail.com>
2024-07-20 22:38:44 +0500

    'update'

--- src/comparison/deepEqual/deepEqual.ts
+++ src/comparison/deepEqual/deepEqual.ts
@@ -6,4 +6,4 @@ import _deepEqual from 'react-fast-compare';
  * General-purpose deep comparison.
  * Also usable in React.memo.
  */
-export const deepEqual = _deepEqual;
+export const deepEqual: (a: unknown, b: unknown) => boolean = _deepEqual;
