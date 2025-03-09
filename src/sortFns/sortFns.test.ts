4777085aa123368b0a42906f9b88d34554d1386d
LesnoyPudge <chuykin.a01@gmail.com>
2025-03-09 16:49:07 +0500

    'update'

--- src/sortFns/sortFns.test.ts
+++ src/sortFns/sortFns.test.ts
@@ -2,12 +2,47 @@ import { sortFns } from './sortFns';
 
 
 
+const toNested = (arr: number[]): Record<'nested', number>[] => {
+    return arr.map((value) => {
+        return {
+            nested: value,
+        };
+    });
+};
+
 const shuffledArray = [4, 2, 5, 1, 3, 3];
 const ascendingArray = [1, 2, 3, 3, 4, 5];
 const descendingArray = [5, 4, 3, 3, 2, 1];
 
+const shuffledArrayNested = toNested(shuffledArray);
+const ascendingArrayNested = toNested(ascendingArray);
+
 describe('sortFns', () => {
-    describe('ascending', () => {
+    describe('<sortFn>.select', () => {
+        it(`
+            should sort non number values by selecting inner number value
+        `, () => {
+            const result = shuffledArrayNested.toSorted(
+                sortFns.ascending.select((ab) => ab.nested),
+            );
+
+            expect(result).toEqual(ascendingArrayNested);
+        });
+
+        it('should exist in every sort function', () => {
+            const isInEvertSortFn = Object.values(sortFns).every((sortFn) => {
+                return 'select' in sortFn;
+            });
+
+            expect(isInEvertSortFn).toBe(true);
+        });
+    });
+
+    describe('ascending/smallToBig', () => {
+        it('should be aliases', () => {
+            expect(sortFns.ascending).toBe(sortFns.smallToBig);
+        });
+
         it('should sort array from lowest to biggest', () => {
             const result = shuffledArray.toSorted(sortFns.ascending);
 
@@ -22,7 +57,12 @@ describe('sortFns', () => {
             expectTypeOf(sortFns.ascending).returns.toBeNumber();
         });
     });
-    describe('descending', () => {
+
+    describe('descending/bigToSmall', () => {
+        it('should be aliases', () => {
+            expect(sortFns.descending).toBe(sortFns.bigToSmall);
+        });
+
         it('should sort array from biggest to lowest', () => {
             const result = shuffledArray.toSorted(sortFns.descending);
 
--- src/sortFns/sortFns.ts
+++ src/sortFns/sortFns.ts
@@ -1,24 +1,75 @@
 
 
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+type SortFnWithSelectors<_Value> = {
+    (a: number, b: number): number;
 
+    /**
+     * Function for deriving number from provided value.
+     */
+    select: <_Value>(
+        selectFn: (ab: _Value) => number
+    ) => (a: _Value, b: _Value) => number;
+};
+
+const withSelectors = <_Value>(
+    fn: (a: number, b: number) => number,
+): SortFnWithSelectors<_Value> => {
+    (fn as SortFnWithSelectors<_Value>).select = (selectFn) => {
+        return (a, b) => fn(selectFn(a), selectFn(b));
+    };
+
+    return fn as SortFnWithSelectors<_Value>;
+};
+
+class SortFns {
+    /**
+     * Sort numbers from smallest to biggest.
+     *
+     * Alias to smallToBig.
+     */
+    ascending = withSelectors((a: number, b: number) => {
+        return a - b;
+    });
+
+    /**
+     * Sort numbers from smallest to biggest.
+     *
+     * Alias to ascending.
+     */
+    smallToBig = this.ascending;
+
+    /**
+     * Sort numbers from biggest to smallest.
+     *
+     * Alias to bigToSmall.
+     */
+    descending = withSelectors((a: number, b: number) => {
+        return b - a;
+    });
+
+    /**
+     * Sort numbers from biggest to smallest.
+     *
+     * Alias to descending.
+     */
+    bigToSmall = this.descending;
+}
 
 /**
  * A collection of sorting functions.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
  */
-export const sortFns = {
-    /**
-     * Sorts numbers from lowest to biggest.
-     */
-    ascending: (a: number, b: number) => {
-        return a - b;
-    },
+// eslint-disable-next-line @typescript-eslint/no-misused-spread
+export const sortFns = { ...new SortFns() };
 
-    /**
-     * Sorts numbers from biggest to lowest.
-     */
-    descending: (a: number, b: number) => {
-        return b - a;
-    },
-};
+const qwe = sortFns.ascending.select<{ v: number }>(
+    // ^?
+    (a) => a.v,
+);
+
+const zxc = [{ v: 5 }, { v: 4 }].sort(
+    // sortFns.ascending.select((a) => a.v, (b) => b.v),
+    qwe,
+);
